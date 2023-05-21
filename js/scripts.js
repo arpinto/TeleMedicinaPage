f82c80e4fa84cefcde1448692ba243cffdc232ed
victtoria11 <victoriapachecolobo@gmail.com>
2023-05-21 17:06:45 -0300

    atualizado

--- js/scripts.js
+++ js/scripts.js
@@ -45,3 +45,18 @@ class MobileNavbar {
     ".nav-list li",
   );
   mobileNavbar.init();
+
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+  
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
