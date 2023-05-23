c4235a9c971a112f8f59bd25a444078dde90614c
victtoria11 <victoriapachecolobo@gmail.com>
2023-05-23 13:56:22 -0300

    header final

--- js/scripts.js
+++ js/scripts.js
@@ -47,22 +47,6 @@ class MobileNavbar {
   );
   mobileNavbar.init();
 
-  var dropdown = document.getElementsByClassName("dropdown-btn");
-  var i;
-  
-  for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var dropdownContent = this.nextElementSibling;
-      if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-      } else {
-        dropdownContent.style.display = "block";
-      }
-    });
-  }
-
-
 // Obtém a lista de elementos de link do cabeçalho
 var headerLinks = document.querySelectorAll('.nav-list li a');
 
